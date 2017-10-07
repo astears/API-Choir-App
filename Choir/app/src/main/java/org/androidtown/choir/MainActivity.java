3f10f69fe0eef21597c0655a0c9af299cdac5350
Andrew Stears <astears@Andrews-MacBook-Pro.local>
2017-10-06 22:07:07 -0700

    added android code

---
+++ Choir/app/src/main/java/org/androidtown/choir/MainActivity.java
@@ -0,0 +1,31 @@
+package org.androidtown.choir;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginButtonPressed(View view) {
+
+        EditText usernameEditText = (EditText)findViewById(R.id.usernameEditText);
+        EditText passwordEditText = (EditText)findViewById(R.id.passwordEditText);
+
+        if (usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")) {
+            Toast.makeText(this, "Both username and password are required", Toast.LENGTH_SHORT).show();
+        } else {
+            Log.i("Username", usernameEditText.getText().toString());
+            Log.i("Password", passwordEditText.getText().toString());
+            Toast.makeText(this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
