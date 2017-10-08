87323f3a92097f9f19936cc3d38f0e99096eb29a
Andrew Stears <astears@Andrews-MacBook-Pro.local>
2017-10-07 18:31:39 -0700

    Created activities for director and member

--- Choir_Android/app/src/main/java/org/androidtown/choir/MainActivity.java
+++ Choir_Android/app/src/main/java/org/androidtown/choir/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +22,24 @@ public class MainActivity extends AppCompatActivity {
         EditText passwordEditText = (EditText)findViewById(R.id.passwordEditText);
         EditText roleEditText = (EditText)findViewById(R.id.roleEditText);
 
+        // Make sure the user enters all fields
         if (usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("") ||
         roleEditText.getText().toString().matches("")) {
             Toast.makeText(this, "Both username, password, and role are required", Toast.LENGTH_SHORT).show();
-        } else {
-            Log.i("Username", usernameEditText.getText().toString());
-            Log.i("Password", passwordEditText.getText().toString());
-            Toast.makeText(this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+        }
+        // Check if the user is a member - Hardcoded for now
+        if (roleEditText.getText().toString().equals("M")) {
+            Intent intent = new Intent(MainActivity.this, MemberActivity.class);
+            startActivity(intent);
+        }
+        else if (roleEditText.getText().toString().equals("D")) {
+            Intent intent = new Intent(MainActivity.this, DirectorActivity.class);
+            startActivity(intent);
+        }
+        else {
+            // Log.i("Username", usernameEditText.getText().toString());
+            // Log.i("Password", passwordEditText.getText().toString());
+            Toast.makeText(this, "Enter 'M' or 'D' for role to login", Toast.LENGTH_SHORT).show();
         }
     }
 }
