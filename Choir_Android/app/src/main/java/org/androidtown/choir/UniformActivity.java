9eaf629fd08db24ee3a4439b106a847b3949e03e
yoonfinity <janghyukyoon@gmail.com>
2017-11-29 09:12:28 -0800

    added dialog box

--- Choir_Android/app/src/main/java/org/androidtown/choir/UniformActivity.java
+++ Choir_Android/app/src/main/java/org/androidtown/choir/UniformActivity.java
@@ -1,6 +1,8 @@
 package org.androidtown.choir;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -11,12 +13,15 @@ import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -158,7 +163,37 @@ public class UniformActivity extends AppCompatActivity
         }
     }
 
-    public void toast_add_button(View view) {
+    public void add_uniforms(View view) {
         Toast.makeText(getApplicationContext(), "Add button clicked!", Toast.LENGTH_LONG).show();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LinearLayout layout = new LinearLayout(getApplicationContext());
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+
+        builder.setTitle("Uniforms");
+        final EditText mensUniformEditText = new EditText(this);
+        layout.addView(mensUniformEditText);
+        mensUniformEditText.setHint("Men's Uniform...");
+
+        final EditText womensUniformEditText = new EditText(this);
+        layout.addView(womensUniformEditText);
+        womensUniformEditText.setHint("Women's Uniform...");
+
+        builder.setView(layout);
+
+
+//        mensUniformEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+//        builder.setView(mensUniformEditText);
+//
+
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                Toast.makeText(getApplicationContext(), "Men: " + mensUniformEditText.getText() + "\nWomen: " + womensUniformEditText.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.show();
     }
 }
